5299f3f59a45ca86abf6b53208c82aa07f305a9e
Shaxobff <xunuddinovshaxob@gmail.com>
2025-09-03 11:07:38 +0500

    add check register

--- bot/main.py
+++ bot/main.py
@@ -26,7 +26,8 @@ async def handle_contact(message: Message) -> None:
     data = {'phone_number':message.contact.phone_number,
             'telegram_id':message.from_user.id,}
     await User.create(**data)
-    # TODO phone yuborgan user, phone ozinikimi? forward ni tekshirish
+
+
 
 
 async def main() -> None:
