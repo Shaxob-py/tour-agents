8c77957f8ba54cabb309984d9caf3aed06fcfdbe
Shaxobff <xunuddinovshaxob@gmail.com>
2025-09-03 10:56:33 +0500

    add register and update db

--- templates/apps/contact.tsx
+++ templates/apps/contact.tsx
@@ -184,7 +184,6 @@ export default function ContactPage() {
           </div>
         </div>
       </section>
-
       <Footer />
     </main>
   )
--- utils/utils.py
+++ utils/utils.py
@@ -15,6 +15,6 @@ def send_telegram_message(chat_id: int, text: str):
 def generate_code() -> int:
     return randint(100000, 999999)
 
-def verification_send_telegram(chat_id: int, code: str):
+def verification_send_telegram(chat_id: int, code: int):
     text = f"🔑 Your verification code is: {code}"
     return send_telegram_message(chat_id, text)
