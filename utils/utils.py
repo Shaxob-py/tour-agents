bae9be6aa23fc90e5e56c9b4c4d61acf4a553acd
aziz <panjievdev@gmai.com>
2025-09-19 11:54:27 +0500

    oijh

--- utils/utils.py
+++ utils/utils.py
@@ -11,7 +11,7 @@ from database import User
 def send_telegram_message(chat_id: int, text: str):
     url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {"chat_id": chat_id, "text": text}
-    response = httpx.post(url, data=payload) # TODO async
+    response = httpx.post(url, data=payload)
     return response.json()
 
 
